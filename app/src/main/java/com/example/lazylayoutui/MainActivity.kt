27956f7d1d4acc6635b7c57f0a33d2adc2265c20
2Kelvin <kvin.mwangi254@gmail.com>
2022-07-26 23:28:23 +0300

    settings icon composable added

--- app/src/main/java/com/example/lazylayoutui/MainActivity.kt
+++ app/src/main/java/com/example/lazylayoutui/MainActivity.kt
@@ -6,13 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.sharp.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.LinearGradient
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lazylayoutui.ui.theme.LazyLayoutUITheme
 
 class MainActivity : ComponentActivity() {
@@ -39,7 +47,15 @@ fun LazyLayout() {
             .fillMaxSize()
             .background(colorResource(R.color.bg_black))
     ) {
-
+        // top settings icon
+        Icon(
+            imageVector = Icons.Sharp.Settings,
+            contentDescription = null,
+            modifier = Modifier
+                .align(Alignment.End)
+                .size(28.dp),
+            tint = Color.White
+        )
     }
 }
 
